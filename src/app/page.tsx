c952a18cd925b89c5509b8b190b2d42c785e5a3e
Omar Khatib <omar.khatib@tourlane.com>
2024-03-26 22:49:59 +0100

    feat: remove tailwindcss add panda

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
-export default function Home() {
-  return <main className='main'>Hello StartWar</main>
+import { css } from '#/css'
+
+export default function Page() {
+  return <main className={css({ fontSize: '2xl', fontWeight: 'bold' })}></main>
 }
